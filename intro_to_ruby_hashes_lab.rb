f302d4f3b51e463737f722482ecaf8b4adef074d
Jeemin Shim <jeemin.shim@gmail.com>
2019-12-28 03:29:50 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,10 @@
 def new_hash
-  # return an empty hash
+  {}
 end
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {:player => "Steph Curry"
+  }
 end
 
 def pioneer
